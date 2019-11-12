5bab08dbeee27c4331f35073eac140a2221ff802
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-11 22:16:30 -0300

    Validating the stuff

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gs_cpf_cnpj/flutter_gs_cpf_cnpj.dart';
+import 'package:flutter_gs_cpf_cnpj/pessoa_fisica.dart';
 import 'package:flutter_gs_cpf_cnpj/pessoa_juridica.dart';
 
 void main() => runApp(MyApp());
@@ -24,20 +25,26 @@ class TesteCpfCnpj extends StatelessWidget {
       body: Column(
         children: <Widget>[
           FutureBuilder<PessoaJuridica>(
-              future: FlutterGSCpfCnpj.buscarPessoaJuridica('10401916000107'),
+              future: FlutterGSCpfCnpj.buscarPessoaJuridica('1040191600010711'),
               builder: (_, snapshot) {
                 if (snapshot.hasData) {
                   print(snapshot.data);
+                  return Text('');
+                } else if(snapshot.hasError) {
+                  print(snapshot.error);
                   return Text('');
                 }
                 return Container();
               },
           ),
           FutureBuilder<PessoaFisica>(
-            future: FlutterGSCpfCnpj.buscarPessoaJuridica('10401916000107'),
+            future: FlutterGSCpfCnpj.buscarPessoaFisica('0400115220111'),
             builder: (_, snapshot) {
               if (snapshot.hasData) {
                 print(snapshot.data);
+                return Text('');
+              } else if(snapshot.hasError) {
+                print(snapshot.error);
                 return Text('');
               }
               return Container();
--- lib/flutter_gs_cpf_cnpj.dart
+++ lib/flutter_gs_cpf_cnpj.dart
@@ -2,6 +2,8 @@ library flutter_gs_cpf_cnpj;
 
 import 'dart:convert';
 
+import 'package:cpf_cnpj_validator/cnpj_validator.dart';
+import 'package:cpf_cnpj_validator/cpf_validator.dart';
 import 'package:http/http.dart' as http;
 
 import 'pessoa_fisica.dart';
@@ -15,20 +17,28 @@ class FlutterGSCpfCnpj {
   static const CNPJ = 'cnpj';
 
   static Future<PessoaJuridica> buscarPessoaJuridica(String cnpj) async {
-    try {
-      final response = await http.get('$BASE_URL:$PORT/$CNPJ/$cnpj');
-      return PessoaJuridica.fromJson(jsonDecode(response.body));
-    } catch(e) {
-      throw e;
+    if (CNPJValidator.isValid(cnpj)) {
+      try {
+        final response = await http.get('$BASE_URL:$PORT/$CNPJ/$cnpj');
+        return PessoaJuridica.fromJson(jsonDecode(response.body));
+      } catch(e) {
+        throw e;
+      }
+    } else {
+      throw Exception('CNPJ inválido');
     }
   }
 
   static Future<PessoaFisica> buscarPessoaFisica(String cpf, [String dtNascimento = '00-00-0000']) async {
-    try {
-      final response = await http.get('$BASE_URL:$PORT/$CPF/$cpf/$dtNascimento');
-      return PessoaFisica.fromJson(jsonDecode(response.body));
-    } catch(e) {
-      throw e;
+    if (CPFValidator.isValid(cpf)) {
+      try {
+        final response = await http.get('$BASE_URL:$PORT/$CPF/$cpf/$dtNascimento');
+        return PessoaFisica.fromJson(jsonDecode(response.body));
+      } catch(e) {
+        throw e;
+      }
+    } else {
+      throw Exception('CPF inválido');
     }
   }
 }
